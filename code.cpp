e2c931bb9eb421f67142a6a34ac0cace16ac00c1
Anshu <aashuraj680@gmail.com>
2026-09-08 21:19:52 +0530

    day 1 of learning c++

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//today i learned what is comment and how comment use 
+//use in vs code .
+/*today i am start to learn c++ with tha help of youtub  ( code with herry )*/
+int main(){
+    cout<<"Hellow world";
+    return 0;
+} 
+//day 1 of learning c++
