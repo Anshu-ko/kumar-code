6cf9a45c9dccd3135d3a7e04462b0856257c667a
Anshu <aashuraj680@gmail.com>
2026-09-10 00:16:03 +0530

    day 2 of learning cpp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    // This line prints the message to the screen
-    cout << "Hello, World!" << endl;
-    
+int main()
+{
+    int num1 , num2;
+    cout<<"Enter tha tha num1 \n";
+    cin>>num1;
+
+    cout<<"Enter tha tha num2 \n";
+    cin>>num2;
+
+    cout << "sum of "<<num1+num2;
+
     return 0;
 }
