d455f61bb807bcb7bf663a1411edb1731b808423
Anshu <aashuraj680@gmail.com>
2026-09-09 19:23:20 +0530

    update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+
 using namespace std;
-//today i learned what is comment and how comment use 
-//use in vs code .
-/*today i am start to learn c++ with tha help of youtub  ( code with herry )*/
 int main(){
-    cout<<"Hellow world";
+    int a =12;
+    int b = 23;
+    cout<<"print tha digit "<<a;
+    
+
+
+
+
+
     return 0;
-} 
-//day 1 of learning c++
+}
