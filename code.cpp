24a5eac5b769cadc834228df4c96a5819ee32889
Anshu <aashuraj680@gmail.com>
2026-09-11 00:34:29 +0530

    day 3 p 3

--- code.cpp
+++ code.cpp
@@ -1,47 +1,18 @@
 #include <iostream>
 using namespace std;
-
-
-int c = 45;
-
-int main()
-{
-   // int a ,b,c ;
-   // cout<<"print tha valua of a :"<<endl;
-    //cin>>a;
+int main(){
+    int age;
+    cout<<"enter tha age "<<endl;
+    cin>>age;
+    if(age>18){
+        cout<<"you are  adult"<<endl;
+    }else if(age==18){
+        cout<<"you are adulting stage"<<endl;
+    }else{
+        cout<<"you are child"<<endl;
+    }
     
-    //cout<<"orint tha value of b :"<<endl;
-    //cin>>b;
-    //c = a +b;
-
-   // cout<<"tha sum c "<<c<<endl;
-
-    //cout<<"tha globle c "<<::c;
-
-   // float d = 34.4f;
-    //cout<<"tha value of d is "<<d<<endl<<"tha value of e is "<<e<<endl;
-    // float x =233;
-     //float & y =x;
-
-
-     //cout<<"print tha "<<x<<endl;
-     //cout<<"print tha "<<y<<endl;
-     
-     //******typecasting ******/
-     int a = 45;
-     float b = 30;
-     cout<<"print tha value of a "<<(float)a<<endl;
-     cout<<"print tha value of a "<<float(a)<<endl;
-
-
-
-     cout<<"print tha value of b "<<(int)b<<endl;
-     cout<<"print tha value of b "<<int(b)<<endl;
-
-
-    
-
-
 
     return 0;
+
 }
