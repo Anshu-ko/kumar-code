11cbfebba89ea42068a008fd89b86bd3a4232dc0
Anshu <aashuraj680@gmail.com>
2026-09-12 01:32:31 +0530

    day 4 of c++

--- code.cpp
+++ code.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
-using namespace std;
-int main(){
-    int age;
-    cout<<"enter tha age "<<endl;
-    cin>>age;
-    if(age>18){
-        cout<<"you are  adult"<<endl;
-    }else if(age==18){
-        cout<<"you are adulting stage"<<endl;
-    }else{
-        cout<<"you are child"<<endl;
-    }
-    
 
-    return 0;
+using namespace std;
+int main()
+{
+
+   // int i =1;
+   // cout<<i<<endl;
+   //syntex for for loop
+  // for(int i=1;  i<40;  i++)
+  // {
+  //  cout<<i<<endl;
+ //  }
+
+ //while loop in syntex
+      //int i = 1 ;
+      //while(i<=30){
+       // cout<<i<<endl;
+       // i++;
+        
+     // }
+    
+    int i = 1;
+    do{
+        cout<<i<<endl;
+        i++;
+    } while(i<=30);
+
+   return 0;
+   
+
 
 }
