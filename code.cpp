d9fa72de165b3873f3c12db4f42b04da9d90bc81
Anshu <aashuraj680@gmail.com>
2026-09-16 22:09:35 +0530

    day 7 of cpp p 2

--- code.cpp
+++ code.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 using namespace std;
 
+ typedef struct employee
+{
+
+  int eId;
+  char favChar;
+  float salary;
+}ep;
+
 int main(){
-  //arry example
-  int mark [4] = {23,34,44,1,};
-  int mathMarks [4];
-  mathMarks [0]=122;
-  mathMarks [1]=33;
-  mathMarks [2]=43;
-  mathMarks [3]=54;
-  cout<<"only marks";
+  ep  anshu;
+  anshu.eId =1;
+  anshu.favChar = 'c';
+  anshu.salary = 100000;
 
-  cout<<mathMarks[0]<<endl;
-  cout<<mathMarks[1]<<endl;
-  cout<<mathMarks[2]<<endl;
-  cout<<mathMarks[3]<<endl;
-  cout<<"marks ";
-  cout<<mark[0]<<endl;
-  cout<<mark[1]<<endl;
-   //you can change tha value of arry ;
-  mark [2] = 4555;
-  cout<<mark[2]<<endl;
-  cout<<mark[3]<<endl;
-
-  int* P = mark;
-  cout<<"tha value of marks [0] is " <<*p<<endl;
-  
- 
-  
-
+  cout<<"tha value is "<<anshu.eId<<endl;
+  cout<<"tha value is "<<anshu.favChar<<endl;
+  cout<<"tha value is "<<anshu.salary<<endl;
   return 0;
 
-  
 }
+
+
+
+
