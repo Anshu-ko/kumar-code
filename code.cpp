82d0a0388d336cdad6967d2ebecc10ab72551b5f
Anshu <aashuraj680@gmail.com>
2026-09-16 21:44:40 +0530

    day 6 of cpp

--- code.cpp
+++ code.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
-
 using namespace std;
-int main() {
 
-  //  what is pointer ? date type which hold tha address of other data typee
-  int a = 3; 
-  int *b = &a;
-  cout<<"tha address of a is"<<&a;
-  cout<<"tha address of a is "<<b;
+int main(){
+  //arry example
+  int mark [4] = {23,34,44,1,};
+  int mathMarks [4];
+  mathMarks [0]=122;
+  mathMarks [1]=33;
+  mathMarks [2]=43;
+  mathMarks [3]=54;
+  cout<<"only marks";
 
-  cout<<"tha address of b is "<<*b;
+  cout<<mathMarks[0]<<endl;
+  cout<<mathMarks[1]<<endl;
+  cout<<mathMarks[2]<<endl;
+  cout<<mathMarks[3]<<endl;
+  cout<<"marks ";
+  cout<<mark[0]<<endl;
+  cout<<mark[1]<<endl;
+   //you can change tha value of arry ;
+  mark [2] = 4555;
+  cout<<mark[2]<<endl;
+  cout<<mark[3]<<endl;
 
-  // pointer to pointer 
-  int**c = &b;
-  cout<<"tha address of b is "<<&b<<endl;
+  int* P = mark;
+  cout<<"tha value of marks [0] is " <<*p<<endl;
+  
+ 
   
 
   return 0;
-}
 
+  
+}
